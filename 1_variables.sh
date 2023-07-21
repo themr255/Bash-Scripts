b1328547b4086fb34cdc93adce4595843f79e395
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-07-21 09:30:39 +0530

    Added - 1_variables.sh

---
+++ 1_variables.sh
@@ -0,0 +1,11 @@
+########################################################################
+#								       #
+# Topic - To declare variables and print those variables               #
+#								       #
+########################################################################
+
+message="Hello World!!, I have started learning shell scripting"
+
+echo $message
+
+printf "\n$message"
