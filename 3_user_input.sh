a810d7e4cec01db1c41a9ba76c2370f2cccb0891
Mahesh Mahajan <115408179+themr255@users.noreply.github.com>
2023-07-23 14:54:05 +0530

    Added - 3_user_input.sh

---
+++ 3_user_input.sh
@@ -0,0 +1,9 @@
+##############################################################################
+#                                                                            #
+# Topic - To take input from user and print the output variables             #
+#                                                                            #
+##############################################################################
+
+read -p "Please enter your username: " user_name
+
+echo "Hello $user_name, how are you?"
